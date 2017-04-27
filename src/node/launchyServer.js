9ab5fc2222a2afbf05c4c6fd1f8ca5f49c44b4bf
Ronald Ray <fritz.ray@eduworks.com>
2017-04-27 10:08:17 -0700

    Error catching.

--- src/node/launchyServer.js
+++ src/node/launchyServer.js
@@ -98,63 +98,66 @@ app.post('/devices', function (request, res) {
 });
 
 app.post('/register', function (request, res) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    console.log("Register:");
-    console.log(request.body);
-    var req = request.body;
-    register(req);
-    webpush.sendNotification(req, JSON.stringify({
-        title: 'Registration successful.'
-    }));
-    res.send();
-});
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
+            console.log("Register:");
+            console.log(request.body);
+            var req = request.body;
+            register(req);
+            webpush.sendNotification(req, JSON.stringify({
+                title: 'Registration successful.'
+            })).catch(function (ex) {
+                    console.log(ex);
+                }; res.send();
+            });
 
-app.post('/send', function (request, res) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    var devices = {};
-    console.log("Send:");
-    console.log(request.body);
-    var req = request.body;
-    if (fs.existsSync("devices.json"))
-        devices = JSON.parse(fs.readFileSync("devices.json"));
-    if (devices[req.subject] === undefined)
-        devices[req.subject] = [];
-    var ary = devices[req.subject];
-    for (var i = 0; i < ary.length; i++)
-        webpush.sendNotification(ary[i], JSON.stringify(req));
-    res.send();
-});
+        app.post('/send', function (request, res) {
+                res.header('Access-Control-Allow-Origin', '*');
+                res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+                res.header('Access-Control-Allow-Headers', 'Content-Type');
+                var devices = {};
+                console.log("Send:");
+                console.log(request.body);
+                var req = request.body;
+                if (fs.existsSync("devices.json"))
+                    devices = JSON.parse(fs.readFileSync("devices.json"));
+                if (devices[req.subject] === undefined)
+                    devices[req.subject] = [];
+                var ary = devices[req.subject];
+                for (var i = 0; i < ary.length; i++)
+                    webpush.sendNotification(ary[i], JSON.stringify(req)).catch(function (ex) {
+                            console.log(ex);
+                        }; res.send();
+                    });
 
-app.post('/unregister', function (request, res) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    console.log("Unregister:");
-    console.log(request.body);
-    var req = request.body;
-    unregister(req);
-    webpush.sendNotification(req, JSON.stringify({
-        title: 'You will no longer receive messages on this device.'
-    }));
-    res.send();
-});
+            app.post('/unregister', function (request, res) {
+                    res.header('Access-Control-Allow-Origin', '*');
+                    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+                    res.header('Access-Control-Allow-Headers', 'Content-Type');
+                    console.log("Unregister:");
+                    console.log(request.body);
+                    var req = request.body;
+                    unregister(req);
+                    webpush.sendNotification(req, JSON.stringify({
+                        title: 'You will no longer receive messages on this device.'
+                    })).catch(function (ex) {
+                            console.log(ex);
+                        }; res.send();
+                    });
 
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("TLA-Launchy Service listening at http://%s:%s", host, port)
-});
-//
-//// This is the same output of calling JSON.stringify on a PushSubscription
-//const pushSubscription = {
-//    "endpoint": "https://gcm-http.googleapis.com/gcm/send",
-//    "keys": {
-//        "auth": "qLAYRzG9TnUwbprns6H2Ew==",
-//        "p256dh": "BILXd-c1-zuEQYXH\\_tc3qmLq52cggfqqTr\\_ZclwqYl6A7-RX2J0NG3icsw..."
-//    }
-//};
-//
+                var server = app.listen(8081, function () {
+                    var host = server.address().address
+                    var port = server.address().port
+                    console.log("TLA-Launchy Service listening at http://%s:%s", host, port)
+                });
+                //
+                //// This is the same output of calling JSON.stringify on a PushSubscription
+                //const pushSubscription = {
+                //    "endpoint": "https://gcm-http.googleapis.com/gcm/send",
+                //    "keys": {
+                //        "auth": "qLAYRzG9TnUwbprns6H2Ew==",
+                //        "p256dh": "BILXd-c1-zuEQYXH\\_tc3qmLq52cggfqqTr\\_ZclwqYl6A7-RX2J0NG3icsw..."
+                //    }
+                //};
+                //
